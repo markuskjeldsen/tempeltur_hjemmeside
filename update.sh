79d239026f3d0734b5767b30d4bd981f0355ede0
Markus Kjeldsen <kinglink224@gmail.com>
2025-04-27 21:13:05 +0200

    enhance update script to reset local changes and pull latest from remote before starting Docker containers

--- update.sh
+++ update.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd /root/tempeltur_hjemmeside/ || exit
+
+# Discard local changes and reset to the latest commit from the remote repository
+git reset --hard
+git clean -f -d
+
+# Pull the latest changes from Git
 git pull
-docker compose up -d --no-deps --build
+
+# Start Docker containers
+docker compose up -d --build
